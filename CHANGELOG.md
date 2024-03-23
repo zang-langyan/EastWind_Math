5738f2f0c6da29141cee64bbdf5acc7ebe3ca4de
Langyan <langyan_zang@163.com>
2024-03-23 22:29:08 +0800

    Basic functionalities provided for Vec and Matrix

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+CHANGELOG
+
+## 2024-3-28
+- Provide the basic functionality for Vector and Matrix operations
---
+++ EastWind_Graphics.h
@@ -0,0 +1,6 @@
+#ifndef EASTWIND_GRAPHICS_H
+#define EASTWIND_GRAPHICS_H
+
+#include "Graphics/utils.h"
+
+#endif // !EASTWIND_GRAPHICS_H
